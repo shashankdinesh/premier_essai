01f30e629feb7ef0f962ba92207f0fe23b17b20f
shashank <shashank.noborders@gmail.com>
2021-06-23 18:40:04 +0530

    Initial commit

---
+++ core/urls.py
@@ -0,0 +1,38 @@
+from django.urls import path
+
+from core import views
+
+app_name = "core"
+
+urlpatterns = [
+    path(
+        "register/",
+        views.RegistrationAPIView.as_view(),
+        name="register"
+    ),
+    path(
+        "upload_contract/",
+        views.UploadContract.as_view(),
+        name="upload_contract"
+    ),
+    path(
+        "recieved_contract/",
+        views.GetRecievedContractListAPIView.as_view(),
+        name="recieved-contract"
+    ),
+    path(
+        "review_contract/",
+        views.GetReviewContractListAPIView.as_view(),
+        name="review-contract"
+    ),
+    path(
+        "update/<int:pk>/",
+        views.UpdateContractDataAPIView.as_view(),
+        name="update-contract"
+    ),
+    path(
+        "login/",
+        views.ObtainTokenLogin.as_view(),
+        name="login",
+    ),
+]
