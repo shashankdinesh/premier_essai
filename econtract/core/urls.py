ec5a437213ba71f920f2e8932df136a2465bc5e5
shashank <shashank.noborders@gmail.com>
2021-06-23 13:40:46 +0530

    -

--- econtract/core/urls.py
+++ econtract/core/urls.py
@@ -1,15 +1,10 @@
 from django.urls import path
 
 from core import views
-from rest_framework.authtoken import views as authtoken_views
 
 app_name = "core"
 
 urlpatterns = [
-    path(
-        "api-token-auth/",
-        authtoken_views.obtain_auth_token
-    ),
     path(
         "register/",
         views.RegistrationAPIView.as_view(),
--- econtract/econtract/settings.py
+++ econtract/econtract/settings.py
@@ -38,9 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework.authtoken',
     'core',
-'widget_tweaks',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
