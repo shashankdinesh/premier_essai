fccab0b302dc76d44d0f06544a3139d62e31bfac
shashank <shashank.noborders@gmail.com>
2021-06-24 13:34:40 +0530

    updated

--- econtract/settings/prod.py
+++ econtract/settings/prod.py
@@ -12,8 +12,8 @@ DATABASES = {
         'NAME': os.environ.get('DATABASE_NAME'),
         'USER': os.environ.get('DATABASE_USER'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
-        'PORT': os.environ.get('DATABASE_PORT', ''),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
 
