f1412096b8022c02145a5e3beab5cce7900b6ff2
shashank <shashank.noborders@gmail.com>
2021-06-24 17:30:26 +0530

    logger added

--- econtract/settings/prod.py
+++ econtract/settings/prod.py
@@ -20,74 +20,74 @@ DATABASES = {
 
 # LOGGER
 # ------------------------------------------------------------------------------
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'filters': {
-#         'require_debug_false': {
-#             '()': 'django.utils.log.RequireDebugFalse'
-#         },
-#         'require_debug_true': {
-#             '()': 'django.utils.log.RequireDebugTrue'
-#         }
-#     },
-#     'formatters': {
-#         'main_formatter': {
-#             'format': '%(levelname)s:%(name)s: '
-#                       '(%(asctime)s; %(pathname)s:%(lineno)d) '
-#                       '%(message)s',
-#             'datefmt': "%Y-%m-%d %H:%M:%S",
-#         },
-#     },
-#     'handlers': {
-#         'mail_admins': {
-#             'level': 'ERROR',
-#             'filters': ['require_debug_false'],
-#             'class': 'django.utils.log.AdminEmailHandler'
-#         },
-#         'console': {
-#             'level': 'DEBUG',
-#             'filters': ['require_debug_true'],
-#             'class': 'logging.StreamHandler',
-#             'formatter': 'main_formatter',
-#         },
-#         'production_file': {
-#             'level': 'INFO',
-#             'class': 'logging.handlers.RotatingFileHandler',
-#             'filename': os.path.join(BASE_DIR,'log', 'main.log'),
-#             'maxBytes': 1024 * 1024 * 1000,  # 200 MB
-#             'backupCount': 12,
-#             'formatter': 'main_formatter',
-#             'filters': ['require_debug_false'],
-#         },
-#         'debug_file': {
-#             'level': 'DEBUG',
-#             'class': 'logging.handlers.RotatingFileHandler',
-#             'filename': os.path.join(BASE_DIR,'log', 'main_debug.log'),
-#             'maxBytes': 1024 * 1024 * 1000,  # 200 MB
-#             'backupCount': 12,
-#             'formatter': 'main_formatter',
-#             'filters': ['require_debug_true'],
-#         },
-#         'null': {
-#             "class": 'logging.NullHandler',
-#         }
-#     },
-#     'loggers': {
-#         'django.request': {
-#             'handlers': ['mail_admins', 'console', 'production_file'],
-#             'level': 'ERROR',
-#             'propagate': True,
-#         },
-#         'django': {
-#             'handlers': ['null', ],
-#         },
-#         'py.warnings': {
-#             'handlers': ['null', ],
-#         },
-#         '': {
-#             'handlers': ['console', 'production_file', 'debug_file'],
-#             'level': "DEBUG",
-#         },
-#     }
-# }
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue'
+        }
+    },
+    'formatters': {
+        'main_formatter': {
+            'format': '%(levelname)s:%(name)s: '
+                      '(%(asctime)s; %(pathname)s:%(lineno)d) '
+                      '%(message)s',
+            'datefmt': "%Y-%m-%d %H:%M:%S",
+        },
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'main_formatter',
+        },
+        'production_file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR,'log', 'main.log'),
+            'maxBytes': 1024 * 1024 * 1000,  # 200 MB
+            'backupCount': 12,
+            'formatter': 'main_formatter',
+            'filters': ['require_debug_false'],
+        },
+        'debug_file': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR,'log', 'main_debug.log'),
+            'maxBytes': 1024 * 1024 * 1000,  # 200 MB
+            'backupCount': 12,
+            'formatter': 'main_formatter',
+            'filters': ['require_debug_true'],
+        },
+        'null': {
+            "class": 'logging.NullHandler',
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins', 'console', 'production_file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'django': {
+            'handlers': ['null', ],
+        },
+        'py.warnings': {
+            'handlers': ['null', ],
+        },
+        '': {
+            'handlers': ['console', 'production_file', 'debug_file'],
+            'level': "DEBUG",
+        },
+    }
+}
