ef46be556e3dcd0c32cc7e51ee6357cf7c8fbb5f
shashank <shashank.noborders@gmail.com>
2021-06-30 14:14:54 +0530

    api's added

--- core/urls.py
+++ core/urls.py
@@ -15,14 +15,14 @@ urlpatterns = [
         views.UploadContract.as_view(),
         name="upload_contract"
     ),
-    path(
-        "recieved_contract/",
-        views.GetRecievedContractListAPIView.as_view(),
-        name="recieved-contract"
-    ),
+    # path(
+    #     "recieved_contract/",
+    #     views.GetRecievedContractListAPIView.as_view(),
+    #     name="recieved-contract"
+    # ),
     path(
         "review_contract/",
-        views.GetReviewContractListAPIView.as_view(),
+        views.GetContractListAPIView.as_view(),
         name="review-contract"
     ),
     path(
@@ -39,5 +39,15 @@ urlpatterns = [
         "upload_contract_file/",
         views.UploadFileS3.as_view(),
         name="upload-contract-file"
+    ),
+    path(
+        "delete_contract_file/",
+        views.DeleteFileS3.as_view(),
+        name="delete-contract-file"
+    ),
+    path(
+        "list_contract_file/",
+        views.ListFileS3.as_view(),
+        name="list-contract-file"
     ),
 ]
