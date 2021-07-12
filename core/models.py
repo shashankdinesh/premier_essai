f63061aec459f4beec7cfca508ae685947d12775
shashank <shashank.noborders@gmail.com>
2021-07-12 17:16:54 +0530

    updated

--- core/models.py
+++ core/models.py
@@ -59,15 +59,15 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(null=True, blank=True, db_index=True)
-    username = models.CharField(max_length=100, unique=True, blank=True, null=True)
+    email = models.EmailField(null=True, unique=True, blank=True, db_index=True)
+    username = models.CharField(max_length=100,  blank=True, null=True)
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=datetime.now)
-    USERNAME_FIELD = "username"
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
     objects = UserManager()
 
